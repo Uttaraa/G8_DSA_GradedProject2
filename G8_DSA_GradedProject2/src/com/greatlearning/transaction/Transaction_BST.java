abbbac91593b1a8818bf41f1761e014d8d3e9348
Uttaraa <142016779+Uttaraa@users.noreply.github.com>
2023-11-20 13:08:55 +0530

    Add files via upload

---
+++ G8_DSA_GradedProject2/src/com/greatlearning/transaction/Transaction_BST.java
@@ -0,0 +1,47 @@
+package com.greatlearning.transaction;
+
+class Node {
+	int val;
+	Node left, right;
+
+	Node(int val) {
+		this.val = val;
+		left = right = null;
+	}
+}
+
+class Transaction_BST {
+	Node node;
+	Node prev = null;
+	Node newHead = null;
+
+	void processBST(Node root) {
+		if (root == null)
+			return;
+		// loop till left leaf
+		processBST(root.left);
+		Node rightNode = root.right;
+		// Node leftNode = root.left;
+		// update new head to hold new root
+		if (newHead == null) {
+			newHead = root;
+			root.left = null;
+			prev = root;
+		} else {
+			// update the right of predecessor and also the predecessor to accomodate next
+			// node
+			prev.right = root;
+			root.left = null;
+			prev = root;
+		}
+		processBST(rightNode);
+	}
+
+	void traverse(Node root) {
+		// traverse till leaf
+		if (root == null)
+			return;
+		System.out.print(root.val + " ");
+		traverse(root.right);
+	}
+}
